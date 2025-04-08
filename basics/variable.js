759eb784c97d9406eaaa31034ac9b25c3a617046
saadDev03 <saadali.webdev@gmail.com>
2025-04-08 06:08:27 +0000

    data types

---
+++ basics/variable.js
@@ -0,0 +1,23 @@
+const accountid = 1234
+let  accountemail = "saasali@gmail.com" ;
+var  accountpassword = 23 ;
+accountcity = "lahore" ;
+let password ;
+
+/*
+prefer not to use var
+because the issue in block scope or functiobnal scope
+
+*/
+
+accountemail = "saad@";
+accountpassword = "45";
+accountcity = "karachi"; 
+
+
+
+console.table([accountid , accountemail , accountpassword , accountcity , password ]);
+
+/*
+conle.table is used to print output in table 
+*/
