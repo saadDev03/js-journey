3f238dfe1e2872f5139e05457c3a92c2255e1e30
saadDev03 <saadali.webdev@gmail.com>
2025-04-08 06:06:24 +0000

    data types

---
+++ basics/data_type.js
@@ -0,0 +1,32 @@
+"use strict" ;  // treat all js code as newwer version  
+
+ //alert(3+3);  we use node js not web browser
+
+ console.log(3 
+    +6) // code readability should be  high
+
+  console.log("saad")
+
+
+  let username = "saad" ; 
+  let userage = 20 ;
+  let isloggedIn = false ; 
+
+  //Data Tyoe
+
+  // number => 2 to power 53
+  // bigint
+  // string => ""
+  // boolen => true/false
+  // null => stand alone value 
+  // undefined
+//symbol => to uniqueness
+
+
+  // object 
+
+  console.log(typeof  undefined );  // its ouput is undefined
+  console.log(typeof  null );  // its output is object 
+  
+
+
